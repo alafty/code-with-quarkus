36386e26508025067dae31cde20c0e955372e0b3
HusseinYasser <husseinyasser388@gmail.com>
2024-08-18 15:12:59 +0300

    added quarkus exception hanlder advice

---
+++ src/main/java/org/acme/exceptions/ControlelrAdvice.java
@@ -0,0 +1,15 @@
+package org.acme.exceptions;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class ControlelrAdvice implements ExceptionMapper<Exception>{
+    
+    @Override
+    public Response toResponse(Exception e)
+    {
+        return Response.status(Response.Status.BAD_REQUEST).entity(new Error(e.getMessage())).build();
+    }
+}
